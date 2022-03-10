01690902ffc5f44738be17ee5a80e4da77307f33
donno2048 <just4now666666@gmail.com>
2022-03-10 19:05:26 +0200

    use smaller types

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #define S(i) (1LL << i)
-#define T(i) argc > i ? strtoll(argv[i], 0, 10) : 1000
+#define T(i) argc > i ? strtol(argv[i], 0, 10) : 1000
 #define U(x) x & 255, x >> 8 & 255
 #define V Y(I), Y(C + 14), Y(H)
 #define W "\x14%c%c%c\x08"X
@@ -13,16 +13,16 @@
 #define Y(x) U(x), x >> 16 & 255, x >> 24 & 255
 #define Z 0, 0, 0, 0
 #define write(...) fprintf(file, __VA_ARGS__)
-int f(long long x[], long long y) {
+int f(long x[], long y) {
 	int output = 0;
 	for (int i = 0; i < 33; i++) output ^= x[i] * (y >> i & 1);
 	return output;
 }
 int main(int argc, char *argv[]) {
-	int i, j;
 	if (argc > 1 && !strcmp(argv[1], "-h")) return printf("Usage: %s   [name of output zip]   [number of files]\t  [size in kilobytes of each file]\nDefaults:\t\toutput.zip\t\t1000\t\t\t\t1000\n", argv[0]);
 	FILE *file = fopen(argc > 1 ? argv[1] : "output.zip", "w");
-	long long A = S(32) - 1, B = T(2), C = T(3), D[33], d[33], E[8], F = C * 1032 + 1, G[33], g[33], H = F;
+	int i, j, B = T(2), C = T(3), F = C * 1032 + 1, H = F;
+	long A = S(32) - 1, D[33], d[33], E[8], G[33], g[33];
 	for (i = 0; i < 33; i++) D[i] = S(i);
 	for (i = 0; i < 8; i++) {
 		E[i] = 0;
@@ -38,11 +38,10 @@ int main(int argc, char *argv[]) {
 		for (i = 0; i < 33; i++) G[i] = f(g, G[i]);
 		F >>= 1;
 	}
-	int I = ~f(D, 2 * A + 1) & A;
-	long long J = write("PK\3\4"W"\1%c%c%c0\xed\xc0\x81\x08%c%c%c\xc0\xb0\xfbS_d\x0b", Z, Z, V, Z, 0, 0);
+	int I = ~f(D, 2 * A + 1) & A, J = write("PK\3\4"W"\1%c%c%c0\xed\xc0\x81\x08%c%c%c\xc0\xb0\xfbS_d\x0b", Z, Z, V, Z, 0, 0);
 	for (i = 0; i < C - 1; i++) J += write("%c", 0);
 	J += write("`");
-	long long K = J;
+	int K = J;
 	for (i = 0; i < B; i++) {
 		char num[100];
 		sprintf(num, "%d", i);
@@ -50,5 +49,6 @@ int main(int argc, char *argv[]) {
 		J += write("PK\1\2\x14%c"W"%c"X, Z, Z, 0, V, U(size), Z, Z, Z, Z) + write(num);
 	}
 	write("PK\5\6%c"X, Z, U(B), U(B), Y(J-K), Y(K), 0, 0);
+	fclose(file);
 	return 0;
 }
